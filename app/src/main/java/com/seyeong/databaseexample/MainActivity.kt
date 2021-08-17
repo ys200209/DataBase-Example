a0e5d4ed2e67e52cf77c17a2963ef4b3a4fbe2f9
ys200209 <lys200209@naver.com>
2021-08-17 21:59:04 +0900

    Create RecyclerView Adapter

--- app/src/main/java/com/seyeong/databaseexample/MainActivity.kt
+++ app/src/main/java/com/seyeong/databaseexample/MainActivity.kt
@@ -2,10 +2,24 @@ package com.seyeong.databaseexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.seyeong.databaseexample.databinding.ActivityMainBinding
+import com.seyeong.databaseexample.databinding.ActivityMainBinding.inflate
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    val helper = SqliteHelper(this, "memo", 1)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val adapter = RecyclerAdapter()
+        adapter.listDate.addAll(helper.selectMemo()) // addAll?. adapter의 listDate에 데이터베이스에서 가져온 데이터를 세팅합니다.
+
+        binding.recyclerMemo.adapter = adapter // 레이아웃.xml의 리사이클러뷰 위젯에 adapter를 연결하고 레이아웃 매니저를 설정한다.
+        binding.recyclerMemo.layoutManager = LinearLayoutManager(this)
+
     }
 }
--- app/src/main/java/com/seyeong/databaseexample/RecyclerAdapter.kt
+++ app/src/main/java/com/seyeong/databaseexample/RecyclerAdapter.kt
@@ -1,4 +1,36 @@
 package com.seyeong.databaseexample
 
-class RecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.seyeong.databaseexample.databinding.ItemRecyclerBinding
+import java.text.SimpleDateFormat
+
+class RecyclerAdapter : RecyclerView.Adapter<Holder>() {
+    var listDate = mutableListOf<Memo>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ItemRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return Holder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return listDate.size
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val memo = listDate.get(position)
+        holder.setMemo(memo)
+    }
+
+}
+
+class Holder(val binding: ItemRecyclerBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun setMemo(memo: Memo) {
+        binding.textNo.text = "${memo.no}"
+        binding.textContent.text = memo.content
+        val sdf = SimpleDateFormat("yyyy/MM/dd hh:mm")
+        binding.textDatetime.text = "${sdf.format(memo.datetime)}"
+    }
 }
