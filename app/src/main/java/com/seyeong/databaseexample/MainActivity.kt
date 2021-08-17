09e5130d2cb79d819b62dafc96d2a38584dfdb2e
ys200209 <lys200209@naver.com>
2021-08-17 22:08:50 +0900

    Connect RecyclerAdapter & SQLite

--- app/src/main/java/com/seyeong/databaseexample/MainActivity.kt
+++ app/src/main/java/com/seyeong/databaseexample/MainActivity.kt
@@ -21,5 +21,15 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerMemo.adapter = adapter // 레이아웃.xml의 리사이클러뷰 위젯에 adapter를 연결하고 레이아웃 매니저를 설정한다.
         binding.recyclerMemo.layoutManager = LinearLayoutManager(this)
 
+        binding.buttonSave.setOnClickListener { // 버튼을 클릭하면.
+            if (binding.editMemo.text.toString().isNotEmpty()) { // 텍스트가 공백이 아니라면
+                val memo = Memo(null, binding.editMemo.text.toString(), System.currentTimeMillis()) // Memo 타입으로 번호에는 null값을, 그리고 메모값과 현재 시각을 넘겨준다.
+
+                helper.insertMemo(memo) // helper 클래스의 insertMemo() 메서드에 앞에서 생성한 memo를 전달해 데이터베이스에 저장합니다.
+                adapter.listDate.clear() // 어댑터의 데이터를 모두 초기화합니다.
+
+                binding.editMemo.setText("") // EditText의 값을 초기화합니다.
+            }
+        }
     }
 }
