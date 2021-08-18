3e9f59917a54865560f4d870002f768c073ce0c6
ys200209 <lys200209@naver.com>
2021-08-18 16:07:19 +0900

    UPDATE SQLite Delete Button

--- app/src/main/java/com/seyeong/databaseexample/RecyclerAdapter.kt
+++ app/src/main/java/com/seyeong/databaseexample/RecyclerAdapter.kt
@@ -6,7 +6,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.seyeong.databaseexample.databinding.ItemRecyclerBinding
 import java.text.SimpleDateFormat
 
-class RecyclerAdapter : RecyclerView.Adapter<Holder>() {
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.Holder>() {
+    var helper : SqliteHelper? = null
     var listDate = mutableListOf<Memo>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
@@ -24,13 +25,25 @@ class RecyclerAdapter : RecyclerView.Adapter<Holder>() {
         holder.setMemo(memo)
     }
 
+    inner class Holder(val binding: ItemRecyclerBinding) : RecyclerView.ViewHolder(binding.root) {
+        var mMemo: Memo? = null // 홀더를 재사용하는 구조때문에 클릭하는 시점에 어떤 데이터가 있는지 알기 위해 setMemo() 메서드로 넘어온 Memo를 임시 저장
+        init {
+            binding.buttonDelete.setOnClickListener {
+                helper?.deleteMemo(mMemo!!)
+                // .deleteMemo 메서드의 매개변수는 null값을 허용하지 않는데 여기로 넘겨주는 mMemo는 null값이 허용된다.
+                // 따라서 null값을 매개변수로 넘겨주는 일을 방지하고자 강제 not null 처리 키워드인 !!로 구현.
+                listDate.remove(mMemo)
+                notifyDataSetChanged()
+            }
+        }
+        fun setMemo(memo: Memo) {
+            binding.textNo.text = "${memo.no}"
+            binding.textContent.text = memo.content
+            val sdf = SimpleDateFormat("yyyy/MM/dd hh:mm")
+            binding.textDatetime.text = "${sdf.format(memo.datetime)}"
+            this.mMemo = memo
+        }
+    }
+
 }
 
-class Holder(val binding: ItemRecyclerBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun setMemo(memo: Memo) {
-        binding.textNo.text = "${memo.no}"
-        binding.textContent.text = memo.content
-        val sdf = SimpleDateFormat("yyyy/MM/dd hh:mm")
-        binding.textDatetime.text = "${sdf.format(memo.datetime)}"
-    }
-}
